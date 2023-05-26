29850973fcba718efaf75bc8115dd40de5f52dea
Deanna Earley <dee@earlsoft.co.uk>
2023-05-26 01:26:17 +0100

    Add python file headers

--- blinkyLib/Constants.py
+++ blinkyLib/Constants.py
@@ -1,2 +1,6 @@
+__copyright__ = "Copyright Earl Software"
+__license__ = "This source code is subject to the BSD 3-Clause license. See Licence.txt for the full licence."
+__author__ = "Deanna Earley"
+
 class Consts:
     DEFAULT_LED_COUNT = 60
