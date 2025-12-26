017b6df991c2afba405c0515545ea1af4ee6e408
Deanna Earley <dee@earlsoft.co.uk>
2025-12-26 17:02:56 +0000

    Update copyright notice

--- blinkyLib/frame.py
+++ blinkyLib/frame.py
@@ -1,4 +1,4 @@
-__copyright__ = "Copyright Earl Software"
+﻿__copyright__ = "Copyright Deanna Earley"
 __license__ = "This source code is subject to the BSD 3-Clause license. See Licence.txt for the full licence."
 __author__ = "Deanna Earley"
 
